fa0cbafd2f0fe978cdc6a560fdae4e0f9eb36f62
JakeReeves <reevesja01@gmail.com>
2022-02-10 12:36:38 -0600

    Passport Facebook Part 1

--- authenticate.js
+++ authenticate.js
@@ -7,6 +7,8 @@ const jwt = require('jsonwebtoken');
 
 const config = require('./config.js');
 
+const FacebookTokenStratgy = require('passport-facebook-token');
+
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
@@ -31,6 +33,37 @@ exports.jwtPassport = passport.use(
                     return done(null, user);
                 } else {
                     return done(null, false);
+                }
+            });
+        }
+    )
+);
+
+exports.facebookPassport = passport.use(
+    new FacebookTokenStrategy(
+        {
+            clientId: confi.facebook.clientId,
+            clientSecret: config.facebook.clientSecret
+        },
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({facebookId: profile.id}, (err, user) => {
+                if (err) {
+                    return done(err, false);
+                }
+                if (!err && user) {
+                    return done(null, user);
+                } else {
+                    user = new User({ username: profile.displayName });
+                    user.facebookId = profile.id;
+                    user.firstname = profile.name.givenName;
+                    user.lastname = profile.name.familyName;
+                    user.save((err, user) => {
+                        if (err) {
+                            return done(err, false);
+                        } else {
+                            return done(null, user);
+                        }
+                    });
                 }
             });
         }
